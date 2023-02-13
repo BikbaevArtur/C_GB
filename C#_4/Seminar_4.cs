9f992e57a28fa907241c8d6eb26fe382f96a497c
bikbaevartur <bigbyar4i@gmail.com>
2023-02-13 21:25:22 +0300

    hw4 29

--- C#_4/Seminar_4.cs
+++ C#_4/Seminar_4.cs
@@ -17,7 +17,7 @@ int num = Convert.ToInt32(Console.ReadLine());
 if( num > 0) int result = Summ(num);
 else Console.WriteLine("-");
 Console.Write(result);
-*/
+
 
 // Задача 2
 // Напишите программу, которая принимает на вход число, и выдает количестно цифр в числе
@@ -36,3 +36,47 @@ int n = Convert.ToInt32(Console.ReadLine());
 if (n > 0) n=n * 1;
 else n = n * -1;
 ColNum(n);
+как вариант добавить новую переменную для того что бы записать овую переменную 
+Math.Abs почитать
+
+
+//Задача 3
+// Напишите программу, которая принимает на вход число N и выдаетпроизведение чисел от 1 до N
+
+void Proz(int num)
+{
+    int result  = 1;
+    for(int i = 1 ; i <= num; i++)
+    {
+    result = result * i; 
+    }
+    Console.WriteLine(result);
+}
+int number = Convert.ToInt32(Console.ReadLine());
+Proz(number);
+*/
+
+// Задача 4 
+// Напишите программу, которая выводит массив из 8 элементов 
+// заполненная 0 , 1 в случайном порядке
+
+int [] CreatNewArray (int size )
+{
+    int [] random01array = new int [size]; // создать переменную, будет хранит целые числа, будет массив, new int резервируем в памяти место
+    for(int i = 0; i < size; i++)
+    {
+    random01array[i] = new Random().Next(0, 2);// правую часть не хватает
+    }
+    return random01array;   
+
+}
+
+void ShowCratArray (int [] array01)
+{
+    for(int i=0; i < array01.Length; i++)
+    {
+        Console.Write(array01[i] +" ");
+    }
+}
+int [] arrayNew = CreatNewArray(8);
+ShowCratArray(arrayNew);
--- Hw_4/homework_4.cs
+++ Hw_4/homework_4.cs
@@ -1,1 +1,63 @@
-﻿
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+/*
+
+void NumDegree (int num, int deg)
+{
+    int i = 0;
+    int result = 1;
+    while(i < deg)
+    {
+        result = result * num;
+        i++;
+    }
+    Console.WriteLine($"Degree {deg} of number{num} = {result}");
+
+}
+
+Console.WriteLine("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input degree: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+
+NumDegree(number,degree);
+*/
+
+//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+/*
+int Summ(int num)
+{
+    int sum = 0;
+    int num1 = num;
+    while(num1 > 0)
+    {
+        sum = num1%10 + sum;
+        num1 = num1/10;
+    }
+    return sum;
+}
+
+Console.WriteLine("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = Summ(number);
+Console.WriteLine($"Summ digits {number} = {result}"); // можно было на минусовые сделать, но времени не хватает =(
+
+*/
+
+// Задача 29: Напишите программу, которая задаёт массив из произвольного кол.во элементов и выводит их на экран.
+
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+//6, 1, 33 -> [6, 1, 33]
+
+
+
+
