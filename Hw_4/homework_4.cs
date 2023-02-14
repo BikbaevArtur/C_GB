c0fd63ecc824dc3e293da774c8f7b9ec3233494d
bikbaevartur <bigbyar4i@gmail.com>
2023-02-14 21:23:26 +0300

    hw4 done

--- Hw_4/homework_4.cs
+++ Hw_4/homework_4.cs
@@ -58,6 +58,27 @@ Console.WriteLine($"Summ digits {number} = {result}"); // можно было н
 
 //6, 1, 33 -> [6, 1, 33]
 
+int [] CreatArray (int size )
+{
+    int [] random = new int [size]; 
+    for(int i = 0; i < size; i++)
+    {
+    random[i] = new Random().Next(0, 100);// 
+    }
+    return random;   
 
+}
+
+void ShowCratArray (int [] array)
+{
+    for(int i=0; i < array.Length; i++)
+    {
+        Console.Write(array[i] +" ");
+    }
+}
+Console.WriteLine("Input size array: ");
+int sizearray = Convert.ToInt32(Console.ReadLine());
+int [] arrayNew = CreatArray(sizearray);
+ShowCratArray(arrayNew);
 
 
