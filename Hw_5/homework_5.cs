46ea4dc57bbae7f80b30be25b53e10e067a83a7a
ArturBikbaev <bigbyar4i@gmail.com>
2023-02-19 18:28:39 +0300

    hw4 done

--- Hw_5/homework_5.cs
+++ Hw_5/homework_5.cs
@@ -17,6 +17,7 @@ void ShowArray(int[] massiv)
 // Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 
 // [345, 897, 568, 234] -> 2
+
 void ResultDigits(int[] massiv)
 {
     int count = 0;
@@ -28,6 +29,9 @@ void ResultDigits(int[] massiv)
     Console.WriteLine();
     Console.WriteLine($"the number of even numbers in array {count}");
 }
+
+/*
+
 
 Console.WriteLine("Input size massiv: ");
 int sizearray = Convert.ToInt32(Console.ReadLine());
@@ -54,7 +58,7 @@ else
 { 
     Console.WriteLine("you entered a non-three-digit number");
 }
-
+*/
 
 
 
@@ -66,5 +70,72 @@ else
 //[3, 7, 23, 12] -> 19
 //[-4, -6, 89, 6] -> 0
 
+int SummOdd(int[] array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++)
+        if(array[i]%2 != 0) sum = array[i]+sum;
+        else sum += 0;
+    return sum;
+}
+
+/*
+
+
+Console.WriteLine("input size array: ");
+int sizearray = Convert.ToInt32(Console.ReadLine());
+Consol.WriteLine("input minDigit array: ");
+int minDigit = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input maxDigit array: ");
+int maxDigit = Convert.ToInt32(Console.ReadLine());
+
+int[] newArray = Array(sizearray, minDigit, maxDigit);
+ShowArray(newArray);
+int result = SummOdd(newArray);
+Console.WriteLine($"summ of odd array numbers: {result}");
+*/
+
 //Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива. Math.Min Math.Max не использовать
 //[3.0 7.1 22.5 2.7 78.8] -> 76.1
+
+
+
+double [] ArrayDouble(int[] array)
+{
+    double[] new_array = new double [array.Length];
+    for(int i=0; i < array.Length; i++) new_array[i] =array[i] + Math.Round(new Random().NextDouble(), 1);
+    return new_array;
+}
+void ShowArrayDouble(double[] massiv)
+{
+    for(int i=0; i < massiv.Length; i++) Console.Write(massiv[i]+ " ");
+}
+void Difference (double[] massiv)
+{
+    double min = massiv[0];
+    double max = massiv[0];
+    for(int i = 1 ; i < massiv.Length; i++)
+    {
+        if(massiv[i]< min) min = massiv[i];
+        if(massiv[i]>max) max = massiv[i];
+        else i++;
+    }
+    Console.WriteLine();
+    Console.WriteLine($"min digits = {min} max digits ={max}");
+    Console.WriteLine($"Difference digits max & min = {Math.Round((max - min),1)}");
+}
+
+/*
+Console.WriteLine("Inpun size array: ");
+int sizearray = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input minDigit array: ");
+int minDigit=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input maxDigit array: ");
+int maxDigit=Convert.ToInt32(Console.ReadLine());
+
+int[] newArray = Array(sizearray, minDigit, maxDigit);
+double[] arne = ArrayDouble(newArray);
+ShowArrayDouble(arne);
+Difference(arne);
+
+*/
