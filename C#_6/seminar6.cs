d1e5779287522cf7ca0772c9d14037335725f8ee
ArturBikbaev <bigbyar4i@gmail.com>
2023-02-24 21:38:45 +0300

    seminar_6done

--- C#_6/seminar6.cs
+++ C#_6/seminar6.cs
@@ -1,6 +1,6 @@
 ﻿//Напишите программу который перевернет одномерный массив
 //
-
+/*
 
 
 int [] NewArray(int size, int minDig, int maxDig)
@@ -40,3 +40,69 @@ ShowArray(array);
 Console.WriteLine();
 int[] sortarray = SortArray (array);
 ShowArray(sortarray);
+*/
+
+// напишите программу которая будет преобразовывать десфтичные числа, в двоичные
+/*
+string BinarSystem(int num)
+{
+    string result = string.Empty;
+    while ( num != 0)
+    {
+        result = num%2 + result;
+        num = num/2;
+    }
+    return result;
+}
+
+Console.WriteLine("input num");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Binar = {BinarSystem(num)}");
+*/
+
+//введите первые N чисел фибоначи. Превые два числа фибаначи 
+/*
+
+int[] FIb (int size, int array0 , int array1)
+{
+    int[] fibArray = new int [size];
+    fibArray[0]= array0;
+    fibArray[1]= array1;
+    for(int i = 2; i < size ; i++)
+    {
+        
+        fibArray[i] = fibArray[i-1] + fibArray[i-2];  
+    }
+    return fibArray;
+}
+
+void ShowArray (int[] array)
+{
+    for(int i=0; i < array.Length; i++)
+    Console.Write(array[i]+ " ");
+}
+
+int size = Convert.ToInt32(Console.ReadLine());
+int array0 = Convert.ToInt32(Console.ReadLine());
+int array1 = Convert.ToInt32(Console.ReadLine());
+int [] fib = FIb(size, array0, array1);
+ShowArray (fib);
+*/
+
+
+// Напишите программу , которая проверяет на вход три числа, и проверяет
+//может ли существовать треугольник со сторонами  такой длинны
+// каждая сторона треугольника, меньше суммы двух сторон 
+void Triangle ( int a, int b , int c)
+{
+    if(a < c+b && b < a+c && c < a+b) 
+        Console.WriteLine("+");
+    else 
+        Console.WriteLine("-"); 
+        
+}
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c =Convert.ToInt32(Console.ReadLine());
+Triangle(a,b,c);
