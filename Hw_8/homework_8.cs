9fa7cb0f78b2e4184c28818cdb0d556528ca7d28
bikbaevartur <bigbyar4i@gmail.com>
2023-03-05 22:26:51 +0300

    hw_8

---
+++ Hw_8/homework_8.cs
@@ -0,0 +1,181 @@
+﻿/*
+Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+int[,] Array2D()
+{
+    Console.WriteLine("Input row array: ");
+    int rowAr = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Input column array: ");
+    int colmnAr = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Input minVol array: ");
+    int minVol = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Input maxVol array: ");
+    int maxVol = Convert.ToInt32(Console.ReadLine());
+
+    int[,] array = new int[rowAr, colmnAr];
+    for (int i = 0; i < rowAr; i++)
+    {
+        for (int j = 0; j < colmnAr; j++)
+        {
+            array[i, j] = new Random().Next(minVol, maxVol + 1);
+        }
+    }
+    return array;
+}
+
+void Show2Darray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+/*
+
+int[,] SortArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1); k++)
+            {
+                if (array[i, j] > array[i, k])
+                {
+                    int temp = array[i, j];
+                    array[i, j] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+
+    return array;
+}
+
+int[,] newarray = Array2D();
+Show2Darray(newarray);
+Console.WriteLine();
+int[,] sortArray = SortArray2D(newarray);
+Show2Darray(sortArray);
+*/
+
+/*
+Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+5 2 6 7
+
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+*/
+
+int[] SumRow(int[,] array)
+{
+    int size = array.GetLength(0);
+    int[] arraysum = new int[size];
+    for (int k = 0; k < arraysum.Length; k++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            int sum = 0;
+            for (int j = 0; j < array.GetLength(1); j++)
+                sum += array[i, j];
+            arraysum[i] = sum;
+        }
+    }
+    return arraysum;
+
+
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+}
+
+void Result(int[] array)
+{
+    int indexmin = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < array[indexmin]) indexmin = i;
+
+    }
+    Console.WriteLine($"the row with the smallest sum of elements: {indexmin}");
+}
+
+
+int[,] newarray = Array2D();
+Show2Darray(newarray);
+int[] sum = SumRow(newarray);
+Console.WriteLine();
+ShowArray(sum);
+Console.WriteLine();
+Result(sum);
+
+/*
+Задача 57: Составить частотный словарь элементов двумерного массива. Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+1, 9, 9, 0,
+2, 8, 0, 9
+0 встречается 2 раза
+1 встречается 1 раз
+2 встречается 1 раз
+8 встречается 1 раз
+9 встречается 3 раза
+*/
+
+
+/*
+
+Задача 58:(дополнительная) Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+
+*/
+
+
+/*
+Задача 60.(дополнительная) ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1)
+*/
+
+/*
+Задача 62ю (дополнительная). Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07
+*/
---
+++ С#_8/lesson_8.cs
@@ -0,0 +1,188 @@
+﻿
+//задайте двумерный массив. Напишите программу, которая поменяет местами любые две строки массива.
+/*
+int [,] Creat2DArray(int row, int column, int minV, int maxV)
+{
+    int[,] array = new int [row,column];
+    for(int i=0; i<row; i++)
+    {
+        for(int j=0; j<column; j++)
+            array[i,j]= new Random().Next(minV, maxV+1);
+    }
+    return array;
+}
+*/
+void Show2Darray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+/*
+int[,] ChangeRows(int[,] array, int row1, int row2)
+{
+    if(row1 >=0 && row1 < array.GetLength(0) && row2>=0 && row2 < array.GetLength (1))
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            int temp = array[row1,j];
+            array[row1,j] = array[row2,j];
+            array[row2,j]= temp;
+        }
+    }
+    else Console.WriteLine("incorrect input");
+    return array;
+
+}
+
+Console.WriteLine("Input row array: ");
+int rowAr = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input column array: ");
+int colmnAr = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input minVol array: ");
+int minVol = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input maxVol array: ");
+int maxVol = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"input number of the first row to remove from 0 to {rowAr-1}");
+int row1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input row2 array: ");
+int row2 = Convert.ToInt32(Console.ReadLine());
+
+
+int[,] array2D = Creat2DArray (rowAr, colmnAr, minVol, maxVol);
+Show2Darray(array2D);
+int[,] arraychenge = ChangeRows(array2D, row1, row2);
+Console.WriteLine();
+Show2Darray(arraychenge);
+*/
+
+// Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы
+// В случае, если это невозможно, программа должна вывести сообщение для пользователей 
+
+int[,] Array2D()
+{
+    Console.WriteLine("Input row array: ");
+    int rowAr = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Input column array: ");
+    int colmnAr = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Input minVol array: ");
+    int minVol = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Input maxVol array: ");
+    int maxVol = Convert.ToInt32(Console.ReadLine());
+
+    int[,] array = new int[rowAr, colmnAr];
+    for (int i = 0; i < rowAr; i++)
+    {
+        for (int j = 0; j < colmnAr; j++)
+        {
+            array[i, j] = new Random().Next(minVol, maxVol + 1);
+        }
+    }
+    return array;
+}
+
+/*
+int[,] CreatArray(int[,] array)
+{
+    if (array.GetLength(0) != array.GetLength(1))
+    {
+        Console.WriteLine("Let it be squar matrix& 4x4 for example ");
+    }
+    else
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = i; j < array.GetLength(1); j++)
+            {
+                int temp = array[i, j];
+                array[i, j] = array[j, i];
+                array[j, i] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+int[,] array1 = Array2D();
+Show2Darray(array1);
+Console.WriteLine();
+int[,] array2 = CreatArray(array1);
+Show2Darray(array2);
+
+
+*/
+
+//Задайте двумерный массив из целых чисел.напишите программу , которая обнулит строку и столбец,
+//на пересечении  которых расположен первый наименьишй элемент массива
+
+int[] MinDigit(int[,] array)
+{
+    int[] indexmin = new int[2];
+    int iMin = 0;
+    int jMin = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] < array[iMin, jMin])
+            {
+                iMin = i;
+                jMin = j;
+            }
+
+
+        }
+    }
+    indexmin[0] = iMin;
+    indexmin[1] = jMin;
+    return indexmin;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+}
+
+
+int [,] CreatArray ( int [,] creatArray, int [] arrayMin)
+{
+    for(int i=0; i<creatArray.GetLength(0); i++)
+    {
+        creatArray[i, arrayMin[1]]=0;
+    }
+    for(int j=0; j<creatArray.GetLength(1); j++)
+    {
+        creatArray[arrayMin[0], j] =0;
+    }
+    
+    return creatArray;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int[,] newarray2D = Array2D();
+Show2Darray(newarray2D);
+Console.WriteLine();
+int[] min = MinDigit(newarray2D);
+ShowArray(min);
+Console.WriteLine();
+int[,] creatarray = CreatArray(newarray2D, min);
+Show2Darray(creatarray);
