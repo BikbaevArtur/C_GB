ad75c6be8c411644a5f28516a8cecc0d590b868b
Artur <bigbyar4i@gmail.com>
2023-03-13 15:49:25 +0500

    hw_9

--- Hw_9/homework_9.cs
+++ Hw_9/homework_9.cs
@@ -40,3 +40,13 @@ Console.WriteLine($"The sum of natural elements between {startNumber} & {endNumb
 //Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 //m = 2, n = 3 -> A(m,n) = 9
 //m = 3, n = 2 -> A(m,n) = 29
+
+int Akker(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return Akker(m - 1, 1);
+    if (m > 0 && n > 0) return Akker(m - 1, Akker(m, n - 1));
+    return Akker(m, n);
+}
+
+Console.WriteLine(Akker(2, 3));
--- С#_9/lesson_9.cs
+++ С#_9/lesson_9.cs
@@ -30,11 +30,26 @@ Console.WriteLine(SumDigit(24));
 
 //Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
 // N < M, N = M, M < N
-
-void Resurs( int n,int m)
+/*
+void ShowNumb(int n, int m)
 {
-    if (n > m) Resurs(n - 1,m);
-    Console.Write(n + " ");   //если поменять местами, будет вывод от n До 1 
+    if (Math.Min(n, m) != Math.Max(n, m)) ShowNumb(Math.Min(n, m) + 1, Math.Max(n, m));
+    Console.Write($"{Math.Min(n, m)} ");
 }
 
-Resurs(45,5);
+ShowNumb(1,-15);
+*/
+
+//Напишите программу которая на вход принимает два числа A and B
+//и возводит число A в целую степень B с помощью рекурсии
+
+// Возведение в степень
+
+double FindPower(int a, int b)
+{
+    if (b > 0) return FindPower(a, b - 1) * a;
+    if (b < 0) return FindPower(a, b + 1) / a;
+    else return 1.0;
+}
+
+Console.WriteLine(FindPower(2, -5));
